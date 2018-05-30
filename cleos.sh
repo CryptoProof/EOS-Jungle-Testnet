ef3823aecb0d27303bb3881fd11c282b32e089af
CryptoProof <tradecrypto1@gmail.com>
2018-05-30 01:04:58 -0400

    Update cleos.sh

--- cleos.sh
+++ cleos.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-################################################################################
-#
-# Scrip Created by http://CryptoLions.io
-# https://github.com/CryptoLions/scripts/
-#
-###############################################################################
 
-NODEOSBINDIR="/home/eos-dawn-v4.0.0/eos/build/programs"
+NODEOSBINDIR="~/eos/build/programs"
 
 WALLETHOST="127.0.0.1"
 NODEHOST="127.0.0.1"
